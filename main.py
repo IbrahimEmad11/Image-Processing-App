4ae2678df8db16584273f4e85d4366feb1ae74a0
Arsany07 <115258356+Arsany07@users.noreply.github.com>
2024-03-12 06:57:41 -0700

    Update main.py
    normalization and equalization

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from task1 import Ui_MainWindow
 from PIL import Image , ImageFilter
 from scipy.ndimage import gaussian_filter, median_filter
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
 # pyuic5 task1.ui -o task1.py
 
@@ -37,6 +39,7 @@ class SignalViewerApp(QMainWindow):
         noisy_image = np.clip(noisy_image + noise, 0, 255).astype(np.uint8)
         return Image.fromarray(noisy_image)
 
+
     def add_salt_and_pepper_noise(self, density=0.01):
         width, height = self.input_image.size
         noisy_image = np.array(self.input_image)
@@ -56,6 +59,38 @@ class SignalViewerApp(QMainWindow):
     def apply_median_filter(self, size=3):
         filtered_img = median_filter(np.array(self.input_image), size=size)
         return Image.fromarray(filtered_img)
+
+    # Equalization
+    def image_equalization(self):
+        # Read the image in grayscale
+        filename = 'Desktop/CV/d.webp'
+        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+        # Compute histogram of the original image
+        hist, bins = np.histogram(img.flatten(), 256, [0,256])
+
+        # Compute cumulative distribution function (CDF)
+        cdf = hist.cumsum()
+        cdf_normalized = cdf * float(hist.max()) / cdf.max()
+
+        # Apply histogram equalization using the CDF
+        cdf_m = np.ma.masked_equal(cdf, 0)
+        cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+        cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+
+        # Map original pixel intensities to new intensities using the CDF
+        img2 = cdf[img]
+        return img2
+        
+
+    # Normalization
+    def image_normalization(self, img):
+        lmin = float(img.min())
+        lmax = float(img.max())
+        x = (img-lmin)
+        y = (lmax-lmin)
+        return np.floor((x/y)*255)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
